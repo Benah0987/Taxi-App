699bd0f5f4f836dac97c64defde79f7fa7719358
Benah0987 <wanyoikebenayah77@gmail.com>
2023-08-17 16:20:08 +0300

    Done

--- app/controllers/drivers_controller.rb
+++ app/controllers/drivers_controller.rb
@@ -1,2 +1,24 @@
 class DriversController < ApplicationController
+    # fetching all the drivers 
+    def index
+        drivers = Driver.all
+        render json: drivers
+    end
+
+    # create action 
+    def create
+        driver = Driver.new(driver_params)
+        if driver.save
+          render json: driver, status: :created
+        else
+          render json: driver.errors, status: :unprocessable_entity
+        end
+    end
+
+    # destroy action
+    def destroy
+        driver = Driver.find(params[:id])
+        driver.destroy
+        head :no_content
+      end
 end
