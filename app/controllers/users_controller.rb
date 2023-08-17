7f3cd94166a4ab4d2ee20a7690667893e5a907d2
Benah0987 <wanyoikebenayah77@gmail.com>
2023-08-17 16:14:54 +0300

    users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,38 @@
 class UsersController < ApplicationController
+    # fetching users 
+    def index
+        users = User.all
+        render json: users
+    end
+
+    # creating a new user 
+    def create
+        user = User.new(user_params)
+        if user.save
+          render json: user, status: :created
+        else
+          render json: user.errors, status: :unprocessable_entity
+        end
+    end
+
+    # updating data of existing user 
+    def update
+        user = User.find(params[:id])
+        if user.update(user_params)
+          render json: user
+        else
+          render json: user.errors, status: :unprocessable_entity
+        end
+    end
+
+    # remove an existing user 
+    def destroy
+        user = User.find(params[:id])
+        user.destroy
+        head :no_content
+    end
+
+    def user_params
+        params.require(:user).permit(:name, :email, :password, :phone_number)
+      end
 end
