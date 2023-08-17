0bd92c4e114e1342785da9d0f4375f208114cae4
Benah0987 <wanyoikebenayah77@gmail.com>
2023-08-17 11:11:24 +0300

    review

---
+++ db/migrate/20230817081055_create_reviews.rb
@@ -0,0 +1,14 @@
+class CreateReviews < ActiveRecord::Migration[7.0]
+  def change
+    create_table :reviews do |t|
+      t.references :user, null: false, foreign_key: true
+      t.references :driver, null: false, foreign_key: true
+      t.references :ride, null: false, foreign_key: true
+      t.integer :rating
+      t.text :comment
+      t.datetime :timestamp
+
+      t.timestamps
+    end
+  end
+end
